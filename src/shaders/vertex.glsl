8cb43e7e2698e9aa4c55a7b8cd62c82eaa94258f
KevinJOH <13-11007@usb.ve>
2025-02-10 18:38:39 -0400

    Movimiento del plano

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -14,14 +14,14 @@ void main() {
 
   if (u_clickTime >= 0.0) {
     float t = u_time - u_clickTime;
-    if (t <= 1.0) {
+    if (t <= 3.0) {
       vec2 clickPos = (u_clickPosition * 2.0 - 1.0) * vec2(projectionMatrix[0][0], projectionMatrix[1][1]);
       float distance = length(pos.xy - clickPos);
       //float wave = sin(50.0 * distance - 5.0 * t) * exp(-5.0 * distance);
-      if (distance < 0.3) { // Asegurándonos de que la deformación se aplique sólo dentro de un radio específico
+      //if (distance < 0.3) { 
         float wave = sin(10.0 * distance - 5.0 * t) * exp(-3.0 * distance);
-        pos.z += wave * 2.0; // Aumentando la magnitud de la deformación para que sea más visible
-      }
+        pos.z += wave * 2.0; 
+      //}
     }
   }
 
